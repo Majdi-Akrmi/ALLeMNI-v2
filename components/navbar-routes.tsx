3ca623cbec6bef3338f95d27e9d95f1bab0eb60b
Majdi Akrmi <majdy.akremi@gmail.com>
2023-12-24 14:58:38 +0100

    Admin Protection

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 import { SearchInput } from "./search-input";
+import { isTeacher } from "@/lib/teacher";
 
 export const NavbarRoutes = () => {
+    const { userId } = useAuth(); 
     const pathname = usePathname();
 
     const isTeacherPage = pathname?.startsWith("/teacher");
@@ -24,18 +26,18 @@ export const NavbarRoutes = () => {
             <div className="flex gap-x-2 ml-auto">
                 {isTeacherPage || isCoursePage ? (
                     <Link href="/">
-                    <Button size="sm" variant="outline">
-                        <LogOut className="h-4 w-4 mr-2"/>
-                        Exit
-                    </Button>
+                        <Button size="sm" variant="outline">
+                            <LogOut className="h-4 w-4 mr-2"/>
+                            Exit
+                        </Button>
                     </Link>
-                ): (
+                ) : isTeacher(userId) ? (
                     <Link href="/teacher/courses">
                         <Button size="sm" variant="outline">
                             Teacher Mode
                         </Button>
                     </Link>
-                )}
+                ) : null}
                 <UserButton 
                     afterSignOutUrl="/"
                 />
